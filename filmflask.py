681d45123301029bf4f9cfd5584106ef092794af
kristaldimayuga <kcdimayuga651@gmail.com>
2024-04-07 22:57:55 +0800

    all features done

--- filmflask.py
+++ filmflask.py
@@ -1,94 +1,177 @@
-movie_library={
-'name':"20 Days in Mariupol",'name':"A Haunting in Venice",
-'name':"American Fiction",
-'name':"American Symphony",
-'name':"Anatomy of a Fall",
-'name':"Ant-Man and the Wasp: Quantumania",
-'name':"Anyone But You",
-'name':"Aquaman and the Lost Kingdom",
-'name':"Barbie",
-'name':"Blue Beetle",
-'name':"Bobi Wine: The People's President",
-'name':"Creed III",
-'name':"Detective Conan: Black Iron Submarine",
-'name':"Dungeons & Dragons: Honor Among Thieves",
-'name':"El Conde",
-'name':"Elemental",
-'name':"Evil Dead Rise",
-'name':"Fast X",
-'name':"Five Nights at Freddy's",
-'name':"Flamin' Hot",
-'name':"Four Daughters",
-'name':"Godzilla Minus One",
-'name':"Golda",
-'name':"Gran Turismo",
-'name':"Guardians of the Galaxy Vol. 3",
-'name':"Haunted Mansion",
-'name':"Indiana Jones and the Dial of Destiny",
-'name':"Insidious: The Red Door",
-'name':"Invincible",
-'name':"Io Capitano",
-'name':"Island in Between",
-'name':"John Wick: Chapter 4",
-'name':"Killers of the Flower Moon",
-'name':"Knight of Fortune",
-'name':"Letter to a Pig",
-'name':"Maestro",
-'name':"May December",
-'name':"Meg 2: The Trench",
-'name':"Migration",
-'name':"Mission: Impossible - Dead Reckoning Part One",
-'name':"Napoleon",
-'name':"Nimona",
-'name':"Ninety-Five Senses",
-'name':"Nyad",
-'name':"Nǎi Nai & Wài Pó",
-'name':"Oppenheimer",
-'name':"Our Uniform",
-'name':"PAW Patrol: The Mighty Movie",
-'name':"Pachyderme",
-'name':"Past Lives",
-'name':"Pathaan",
-'name':"Perfect Days",
-'name':"Poor Things",
-'name':"Red, White and Blue",
-'name':"Robot Dreams",
-'name':"Rustin",
-'name':"Saw X",
-'name':"Scream VI",
-'name':"Shazam! Fury of the Gods",
-'name':"Society of the Snow",
-'name':"Sound of Freedom",
-'name':"Spider-Man: Across the Spider-Verse",
-'name':"Taylor Swift: The Eras Tour",
-'name':"Teenage Mutant Ninja Turtles: Mutant Mayhem",
-'name':"The ABCs of Book Banning",
-'name':"The After",
-'name':"The Barber of Little Rock",
-'name':"The Boy and the Heron",
-'name':"The Color Purple",
-'name':"The Creator",
-'name':"The Equalizer 3",
-'name':"The Eternal Memory",
-'name':"The Exorcist: Believer",
-'name':"The Flash",
-'name':"The Holdovers",
-'name':"The Hunger Games: The Ballad of Songbirds & Snakes",
-'name':"The Last Repair Shop",
-'name':"The Little Mermaid",
-'name':"The Marvels",
-'name':"The Nun II",
-'name':"The Super Mario Bros. Movie",
-'name':"The Teachers' Lounge",
-'name':"The Wonderful Story of Henry Sugar",
-'name':"The Zone of Interest",
-'name':"To Kill a Tiger",
-'name':"Transformers: Rise of the Beasts",
-'name':"Trolls Band Together",
-'name':"Wish",
-'name':"Wonka"
+movie_library = {
+    'title1': "Transformers: Rise of the Beasts",
+    'title2': "Society of the Snow",
+    'title3': "Oppenheimer",
+    'title4': "John Wick: Chapter 4",
+    'title5': "The Exorcist: Believer",
+    'title6': "Maestro",
+    'title7': "Evil Dead Rise",
+    'title8': "Anatomy of a Fall",
+    'title9': "Dungeons & Dragons: Honor Among Thieves",
+    'title10': "Napoleon",
+    'title11': "American Fiction",
+    'title12': "The Little Mermaid",
+    'title13': "Anyone But You",
+    'title14': "The After",
+    'title15': "Indiana Jones and the Dial of Destiny",
+    'title16': "Knight of Fortune",
+    'title17': "Godzilla Minus One",
+    'title18': "Pathaan",
+    'title19': "Saw X",
+    'title20': "Gran Turismo",
+    'title21': "The Creator",
+    'title22': "Flamin' Hot",
+    'title23': "Blue Beetle",
+    'title24': "Barbie",
+    'title25': "The ABCs of Book Banning",
+    'title26': "The Flash",
+    'title27': "A Haunting in Venice",
+    'title28': "Pachyderme",
+    'title29': "Io Capitano",
+    'title30': "Perfect Days",
+    'title31': "Golda",
+    'title32': "Letter to a Pig",
+    'title33': "Nǎi Nai & Wài Pó",
+    'title34': "The Hunger Games: The Ballad of Songbirds & Snakes",
+    'title35': "The Last Repair Shop",
+    'title36': "The Nun II",
+    'title37': "Five Nights at Freddy's",
+    'title38': "Ninety-Five Senses",
+    'title39': "The Holdovers",
+    'title40': "The Equalizer 3",
+    'title41': "The Marvels",
+    'title42': "Creed III",
+    'title43': "Rustin",
+    'title44': "Mission: Impossible - Dead Reckoning Part One",
+    'title45': "The Wonderful Story of Henry Sugar",
+    'title46': "Sound of Freedom",
+    'title47': "To Kill a Tiger",
+    'title48': "The Boy and the Heron",
+    'title49': "Robot Dreams",
+    'title50': "Insidious: The Red Door",
+    'title51': "Invincible",
+    'title52': "El Conde",
+    'title53': "Taylor Swift: The Eras Tour",
+    'title54': "Io Capitano",
+    'title55': "The Color Purple",
+    'title56': "The Eternal Memory",
+    'title57': "Ant-Man and the Wasp: Quantumania",
+    'title58': "Our Uniform",
+    'title59': "Wish",
+    'title60': "Guardians of the Galaxy Vol. 3",
+    'title61': "Trolls Band Together",
+    'title62': "Nimona",
+    'title63': "Fast X",
+    'title64': "Spider-Man: Across the Spider-Verse",
+    'title65': "Detective Conan: Black Iron Submarine",
+    'title66': "The Teachers' Lounge",
+    'title67': "The Zone of Interest",
+    'title68': "The Barber of Little Rock",
+    'title69': "The Super Mario Bros. Movie",
+    'title70': "Meg 2: The Trench",
+    'title71': "Haunted Mansion",
+    'title72': "Shazam! Fury of the Gods",
+    'title73': "The Wonderful Story of Henry Sugar",
+    'title74': "Past Lives",
+    'title75': "Bobi Wine: The People's President",
+    'title76': "Migration",
+    'title77': "Teenage Mutant Ninja Turtles: Mutant Mayhem",
+    'title78': "Red, White and Blue",
+    'title79': "Napoleon",
+    'title80': "Twenty Days in Mariupol",
+    'title81': "The Holdovers",
+    'title82': "Elemental",
+    'title83': "Pachyderme",
+    'title84': "Twenty Days in Mariupol",
+    'title85': "The ABCs of Book Banning",
+    'title86': "Twenty Days in Mariupol",
+    'title87': "The ABCs of Book Banning",
+    'title88': "Twenty Days in Mariupol",
+    'title89': "Twenty Days in Mariupol"
 }
 
+oscars = {
+    'title1': "20 Days in Mariupol",
+    'title2': "American Fiction",
+    'title3': "American Symphony",
+    'title4': "Anatomy of a Fall",
+    'title5': "Barbie",
+    'title6': "Bobi Wine: The People's President",
+    'title7': "El Conde",
+    'title8': "Elemental",
+    'title9': "Flamin' Hot",
+    'title10': "Four Daughters",
+    'title11': "Godzilla Minus One",
+    'title12': "Golda",
+    'title13': "Guardians of the Galaxy Vol. 3",
+    'title14': "Indiana Jones and the Dial of Destiny",
+    'title15': "Invincible",
+    'title16': "Io Capitano",
+    'title17': "Island in Between",
+    'title18': "Killers of the Flower Moon",
+    'title19': "Knight of Fortune",
+    'title20': "Letter to a Pig",
+    'title21': "Maestro",
+    'title22': "May December",
+    'title23': "Mission: Impossible - Dead Reckoning Part One",
+    'title24': "Napoleon",
+    'title25': "Nimona",
+    'title26': "Ninety-Five Senses",
+    'title27': "Nyad",
+    'title28': "Nǎi Nai & Wài Pó",
+    'title29': "Oppenheimer",
+    'title30': "Our Uniform",
+    'title31': "Pachyderme",
+    'title32': "Past Lives",
+    'title33': "Perfect Days",
+    'title34': "Poor Things",
+    'title35': "Red, White and Blue",
+    'title36': "Robot Dreams",
+    'title37': "Rustin",
+    'title38': "Society of the Snow",
+    'title39': "Spider-Man: Across the Spider-Verse",
+    'title40': "The ABCs of Book Banning",
+    'title41': "The After",
+    'title42': "The Barber of Little Rock",
+    'title43': "The Boy and the Heron",
+    'title44': "The Color Purple",
+    'title45': "The Creator",
+    'title46': "The Eternal Memory",
+    'title47': "The Holdovers",
+    'title48': "The Last Repair Shop",
+    'title49': "The Teachers' Lounge",
+    'title50': "The Wonderful Story of Henry Sugar",
+    'title51': "The Zone of Interest",
+    'title52': "To Kill a Tiger"
+}
+
+hgross = {
+    'title1': "Barbie",
+    'title2': "The Super Mario Bros. Movie",
+    'title3': "Oppenheimer",
+    'title4': "Guardians of the Galaxy Vol. 3",
+    'title5': "Fast X",
+    'title6': "Spider-Man: Across the Spider-Verse",
+    'title7': "Wonka",
+    'title8': "The Little Mermaid",
+    'title9': "Mission: Impossible - Dead Reckoning Part One",
+    'title10': "Elemental",
+    'title11': "Ant-Man and the Wasp: Quantumania",
+    'title12': "John Wick: Chapter 4",
+    'title13': "Transformers: Rise of the Beasts",
+    'title14': "Aquaman and the Lost Kingdom",
+    'title15': "Meg 2: The Trench",
+    'title16': "Indiana Jones and the Dial of Destiny",
+    'title17': "The Hunger Games: The Ballad of Songbirds & Snakes",
+    'title18': "Migration",
+    'title19': "Five Nights at Freddy's",
+    'title20': "Creed III",
+    'title21': "The Flash",
+    'title22': "The Nun II",
+    'title23': "Taylor Swift: The Eras Tour",
+    'title24': "Wish",
+    'title25': "Sound of Freedom"
+}
 
 user_info={}
 
@@ -117,8 +200,178 @@ def login():
         print("invalid password/ account does not exist.")
         return
     
-def u_menu():
-    pass
+def u_profile(username):
+    rated_movies = sum('rating' in movie_data for movie_data in user_info.get(username, {}).values())
+    reviewed_movies = sum('review' in movie_data for movie_data in user_info.get(username, {}).values())
+    watchlist_count = sum('watchlist' in movie_data for movie_data in user_info.get(username, {}).values())
+    
+    print(f"\nUser ({username}) Profile")
+    print(f"{rated_movies} movies rated")
+    print(f"{reviewed_movies} movies reviewed")
+    print(f"{watchlist_count} movies in watchlist")
+
+    choice=int(input("\nEnter 1 to go back to menu: "))
+    if choice==1:
+        return
+
+def movielist(username):
+    while True:
+        print("Sort movies by:")
+        print("1. All Movies")
+        print("2. A-Z")
+        print("3. Oscar nominated")
+        print("4. Highest Grossing")
+        print("5.Return")
+
+        choice=int(input("\nEnter your choice: "))
+
+        try:
+            if choice==1:
+                print("Best of 2023")
+                for title, movie_name in movie_library.items():
+                    print(f'-{movie_name}')
+                movie_choice = input("\nEnter the movie you want to view details (Type the full name exactly as shown). Press 1 to go back: ")
+                if movie_choice in movie_library.values():
+                    moviechoice_menu(username, movie_choice)
+                elif movie_choice==1:
+                    return
+                else:
+                    print("Movie not found in the list.")
+
+            elif choice==2:
+                print("A-Z Best of 2023")
+                sortedlist=sorted(movie_library.values())
+                for movie_name in sortedlist:
+                    print(f'-{movie_name}')
+                movie_choice = input("\nEnter the movie you want to view details (Type the full name exactly as shown). Press 1 to go back: ")
+                if movie_choice in movie_library.values():
+                    moviechoice_menu(username, movie_choice)
+                elif movie_choice==1:
+                    return
+                else:
+                    print("Movie not found in the list.")
+
+            elif choice==3:
+                print("Oscar Nominated Movies of 2023")
+                for title, movie_name in oscars.items():
+                    print(f'-{movie_name}')
+                movie_choice = input("\nEnter the movie you want to view details (Type the full name exactly as shown). Press 1 to go back: ")
+                if movie_choice in oscars.values():
+                    moviechoice_menu(username, movie_choice)
+                elif movie_choice==1:
+                    return
+                else:
+                    print("Movie not found in the list.")
+
+            elif choice==4:
+                print("Top 25 Highest Grossing Films of 2023")
+                for i, (key, movie_name) in enumerate(hgross.items(), 1):
+                    print(f'{i}. {movie_name}')
+                movie_choice = input("\nEnter the movie you want to view details (Type the full name exactly as shown). Press 1 to go back: ")
+                if movie_choice in hgross.values():
+                    moviechoice_menu(username, movie_choice)
+                elif movie_choice==1:
+                    return
+                else:
+                    print("Movie not found in the list.")
+
+            elif choice==5:
+                return
+    
+            else:
+                print("\nInvalid number. Please enter a number between 1-5.")
+        except ValueError:
+            print("\nInvalid input. Please enter a number.")
+
+def my_flask(username):
+    while True:
+        print("\nRated Movies:")
+        for movie, data in user_info.get(username,{}).items():
+            if 'rating' in data:
+                print(f"{movie}: {data['rating']} stars")
+        print("\fReviewed Movies:")
+        for movie, data in user_info.get(username,{}).items():
+            if 'review' in data:
+                print(f"{movie}: {data['review']}")
+        print("\nWatchlist")
+        for movie, data in user_info.get(username, {}).items():
+            if 'watchlist' in data:
+                print(f"- {movie}")
+        
+        choice=int(input("back (enter 1): "))
+        if choice==1:
+            return
+        else:
+            print("invalid input")
+
+def moviechoice_menu(username, movie_choice):
+    while True:
+        print(f'Movie: {movie_choice}')
+        print("1. Rate")
+        print("2. Review")
+        print("3. Add to watchlist")
+        print("4. View my flask")
+        print("5. Back")
+        
+        menu_choice = int(input("Enter your choice: "))
+        try:
+            if menu_choice == 1:
+                rating = int(input("Enter your rating (1-5 stars): "))
+                if 1 <= rating <= 5:
+                    if username not in user_info:
+                        user_info[username] = {}
+                    user_info[username].setdefault(movie_choice, {})['rating'] = rating
+                    print("Rating recorded successfully.")
+                else:
+                    print("Invalid rating. Please enter a rating between 1 and 5.")
+    
+            elif menu_choice == 2:
+                review = input("Write your review: ")
+                if username not in user_info:
+                    user_info[username] = {}
+                user_info[username].setdefault(movie_choice, {})['review'] = review
+                print("Review recorded successfully.")
+
+            elif menu_choice == 3:
+                if username not in user_info:
+                    user_info[username] = {}
+                user_info[username].setdefault(movie_choice, {})['watchlist'] = True
+                print(f"{movie_choice} added to watchlist.")
+            
+            elif menu_choice == 4:
+                my_flask(username)
+
+            elif menu_choice == 5:
+                return
+                
+            else:
+                print("Invalid choice. Please enter a number between 1 and 4.")
+        except ValueError:
+            print("\nInvalid input. Please enter a number.")
+
+def u_menu(username):
+    while True:
+        print("\nMovie Dashboard")
+        print("Here are the most popular movies of the week:")
+        print("1. Poor Things\n2.Barbie\n3. Oppenheimer\n4.The Super Mario Bros. Movie\n5.Spider-Man: Across the Spider-Verse")
+        print("\nLoad your flask:")
+        print("1. View Profile")
+        print("2. View Movies")
+        print("3. Log-out")
+
+        choice=int(input("Enter your choice: "))
+
+        try:
+            if choice==1:
+                u_profile(username)
+            elif choice==2:
+                movielist(username)
+            elif choice==3:
+                return
+            else:
+                print("\nInvalid choice. Please enter a number between 1-3.")
+        except ValueError:
+            print("\nInvalid input. Please enter a number.")
 
 def main():
     while True:
